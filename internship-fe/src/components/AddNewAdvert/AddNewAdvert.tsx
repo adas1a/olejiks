e2e81aed2d2957ab03bc4cd656704ec95e52d66e
Mikorolo <mikolaj.widanka@gmail.com>
2021-05-23 19:51:53 +0200

    added validation and submission

--- internship-fe/src/components/AddNewAdvert/AddNewAdvert.tsx
+++ internship-fe/src/components/AddNewAdvert/AddNewAdvert.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { useFormik } from 'formik'
-import {unstable_renderSubtreeIntoContainer} from "react-dom";
+import { FormikErrors } from 'formik';
 
+interface FormValues {
+  advertTitle: string;
+  category: string;
+  description: string;
+  location: string;
+  person: string;
+  photos: any;
+  number: string;
+  accept: boolean;
+}
 
 const AddNewAdvert = () => {
 
@@ -14,38 +24,69 @@ const AddNewAdvert = () => {
       description: '',
       location:'',
       person:'',
-      number:'',
-      checkbox:''
+      number: '',
+      accept: false
     },
     onSubmit: values => {
-      // alert(JSON.stringify(values, null, 2));
+      console.log(values);
     },
+    validate: (values: FormValues) => {
+      let errors: FormikErrors<FormValues> = {};
+      if (!values.advertTitle) {
+        errors.advertTitle = 'Required';
+      }
+      if (!values.category) {
+        errors.category = 'Required';
+      }
+      if (!values.description) {
+        errors.description = 'Required';
+      }
+      if (!values.location) {
+        errors.location = 'Required';
+      }
+      if (!values.person) {
+        errors.person = 'Required';
+      }
+      if (!values.accept) {
+        errors.accept = 'Required';
+      }
+      let regex =  /^\d{9}$/;
+      if (!values.number) {
+        errors.number = 'Required';
+      }
+      else if (!regex.test(values.number)) {
+        errors.number = 'Invalid phone number'
+      }
+
+      return errors;
+    }
+
   });
-  console.log(formik.values)
+
 
   return (
 
     <Container>
-      <Form className={'m-5'}>
+      <Form className={'m-5'} onSubmit={formik.handleSubmit}>
 
         <Form.Group controlId="advertTitle">
           <Form.Label>Advert Title</Form.Label>
           <Form.Control
-              type="text"
-              name='advertTitle'
-              placeholder="iphone 11 plus"
-              onChange={formik.handleChange}
-              value={formik.values.advertTitle}
+            type="text"
+            name='advertTitle'
+            placeholder="iphone 11 plus"
+            onChange={formik.handleChange}
+            value={formik.values.advertTitle}
           />
         </Form.Group>
 
         <Form.Group controlId="category">
           <Form.Label>Category</Form.Label>
           <Form.Control
-              as="select"
-              name='category'
-              onChange={formik.handleChange}
-              value={formik.values.category}
+            as="select"
+            name='category'
+            onChange={formik.handleChange}
+            value={formik.values.category}
           >
             <option>1</option>
             <option>2</option>
@@ -58,61 +99,60 @@ const AddNewAdvert = () => {
         <Form.Group controlId="photos">
           <Form.Label>Photos</Form.Label>
           <Form.File
-              name='photos'
-              onChange={formik.handleChange}
-              value={formik.values.photos}
+            name='photos'
+            onChange={formik.handleChange}
+            value={formik.values.photos}
           />
         </Form.Group>
 
         <Form.Group controlId="description">
           <Form.Label>Description</Form.Label>
           <Form.Control
-              as="textarea"
-              rows={5}
-              name='description'
-              onChange={formik.handleChange}
-              value={formik.values.description}
+            as="textarea"
+            rows={5}
+            name='description'
+            onChange={formik.handleChange}
+            value={formik.values.description}
           />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Location</Form.Label>
           <Form.Control
-              type='text'
-              name='location'
-              onChange={formik.handleChange}
-              value={formik.values.location}
+            type='text'
+            name='location'
+            onChange={formik.handleChange}
+            value={formik.values.location}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Seller Name</Form.Label>
           <Form.Control
-              type='text'
-              name='person'
-              onChange={formik.handleChange}
-              value={formik.values.person}
+            type='text'
+            name='person'
+            onChange={formik.handleChange}
+            value={formik.values.person}
           />
         </Form.Group>
         <Form.Group>
           <Form.Label>Number</Form.Label>
           <Form.Control
-              type='number'
-              name='number'
-              onChange={formik.handleChange}
-              value={formik.values.number}
+            type='number'
+            name='number'
+            onChange={formik.handleChange}
+            value={formik.values.number}
           />
         </Form.Group>
 
         <Form.Group controlId="accept">
           <Form.Check
-              type="checkbox"
-              label="accept term & conditions"
-              onChange={formik.handleChange}
-              value={formik.values.checkbox}
+            type="checkbox"
+            label="accept term & conditions"
+            onChange={formik.handleChange}
           />
         </Form.Group>
 
-        <Button variant="primary" type="submit">Review Advert</Button>
+        <Button variant="primary">Review Advert</Button>
 
         <Button variant="primary" type="submit">Submit</Button>
 
